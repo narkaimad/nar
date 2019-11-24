b473ee24ab43130cf913dfab52896f88ba30a2db
narkaimad <58130400+narkaimad@users.noreply.github.com>
2019-11-24 13:48:02 +0700

    Update bot.php
    'YOUR-CHANNEL-ACCESS-TOKEN'
    'YOUR-CHANNEL-SECRET-CODE'

--- bot.php
+++ bot.php
@@ -9,8 +9,8 @@ Modified @ Farzain - zFz
 require_once('./line_class.php');
 require_once('./unirest-php-master/src/Unirest.php');
 
-$channelAccessToken = 'YOUR-CHANNEL-ACCESS-TOKEN'; //sesuaikan 
-$channelSecret = 'YOUR-CHANNEL-SECRET-CODE';//sesuaikan
+$channelAccessToken = ; //sesuaikan 
+$channelSecret = ;//sesuaikan
 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 
